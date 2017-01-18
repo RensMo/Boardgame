e60ba40071dd731ebfeaabf5196ec57ca21ffbc5
Bahadir Aliyazicioglu <0932710@hr.nl>
2017-01-18 15:47:47 +0100

    Test voor meerdere personen

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ print("Laurus is the one and only third leg man.")
 print("Hooray")
 
 print("Ik ben er ook bij, hihihihihi (Bahadir)")
+
+print("Dit is een test om te kijken wat er gebeurd als meerdere personen in het zelfde bestand werken, en deze vervolgens beide proberen op te slaan. "
+      "Wij danken u voor uw geduld om dit te lezen, aangezien dit nergens over gaat. Het verbaasd ons nog steeds dat u verder leest. "
+      "Aan u de keuze natuurlijk om hier verder mee te gaan. Doei")
