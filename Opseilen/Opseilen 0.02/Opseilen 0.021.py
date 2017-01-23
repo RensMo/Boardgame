53845b2994976d35c7a0129380612b0ceb55df6f
Bahadir Aliyazicioglu <0932710@hr.nl>
2017-01-23 13:22:01 +0100

    Buttons changed

--- Opseilen/Opseilen 0.02/Opseilen 0.021.py
+++ Opseilen/Opseilen 0.02/Opseilen 0.021.py
@@ -69,10 +69,10 @@ class Menu:
         self.I = pygame.transform.scale(self.I, (int(x), int(y)))
         self.rect = pygame.Rect((0,0), (x, y))
 
-        self.B1 = Button(self.x * 0.12, self.y * 0.29, self.x * 0.2, self.y * 0.14, I1)
-        self.B2 = Button(self.x * 0.12, self.y * 0.44, self.x * 0.2, self.y * 0.14, I2)
-        self.B3 = Button(self.x * 0.12, self.y * 0.59, self.x * 0.2, self.y * 0.14, I3)
-        self.B4 = Button(self.x * 0.12, self.y * 0.74, self.x * 0.2, self.y * 0.14, I4)
+        self.B1 = Button(self.x * 0.12, self.y * 0.29, self.x * 0.23, self.y * 0.14, I1)
+        self.B2 = Button(self.x * 0.12, self.y * 0.44, self.x * 0.23, self.y * 0.14, I2)
+        self.B3 = Button(self.x * 0.12, self.y * 0.59, self.x * 0.23, self.y * 0.14, I3)
+        self.B4 = Button(self.x * 0.12, self.y * 0.74, self.x * 0.23, self.y * 0.14, I4)
 
     def draw(self, surface):
         surface.blit(self.I, (self.rect))
