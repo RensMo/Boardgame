09598ec121406a039da2118c964b2ef77b0eca29
Endy Hu <0937452@hr.nl>
2017-01-24 14:11:18 +0100

    Add files via upload

---
+++ Opseilen/Opseilen 0.03/Text.py
@@ -0,0 +1,36 @@
+import pygame
+from Image import *
+
+class Font:
+    pygame.init()
+    pygame.font.init()
+    font = pygame.font.Font(None, 24)
+
+class Text: ###klopt nog niet
+    def __init__(self, x, y, sx, sy, I):
+        self.x = x
+        self.y = y
+        self.sx = sx
+        self.sy = sy
+        self.I = I
+        self.I = pygame.transform.scale(self.I, (int(sx), int(sy)))
+        self.rect = pygame.Rect((x,y), (sx,sy))
+        self.screct = pygame.Surface((int(sx), int(sy)))
+
+    def draw(self, surface):
+        surface.blit(self.I, (self.rect))
+
+class Textbg:
+    def __init__(self, x, y, sx, sy, I):
+        self.x = x
+        self.y = y
+        self.sx = sx
+        self.sy = sy
+        self.I = I
+        self.I = pygame.transform.scale(self.I, (int(sx), int(sy)))
+        self.rect = pygame.Rect((x,y), (sx,sy))
+        self.screct = pygame.Surface((int(sx), int(sy)))
+
+    def draw(self, surface):
+        surface.blit(self.I, (self.rect))
+
