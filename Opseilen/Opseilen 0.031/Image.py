6aabde4d4d5f1a046a4aa509fff9e8a3f2bb0663
Endy Hu <0937452@hr.nl>
2017-01-24 15:00:09 +0100

    Add files via upload

---
+++ Opseilen/Opseilen 0.031/Image.py
@@ -0,0 +1,27 @@
+import pygame
+
+###BACKGROUNDS
+I0 = pygame.image.load("Assets/Background_S0.jpg")
+I8 = pygame.image.load("Assets/text_bg.png")
+I12 = pygame.image.load("Assets/Play/p1_bg.jpg")
+I13 = pygame.image.load("Assets/Play/p2_bg.jpg")
+
+###BUTTONS
+I1 = pygame.image.load("Assets/Play_Button.jpg")
+I2 = pygame.image.load("Assets/Help_Button.jpg")
+I3 = pygame.image.load("Assets/Settings_Button.jpg")
+I4 = pygame.image.load("Assets/Quit_Button.jpg")
+I5 = pygame.image.load("Assets/Back_Button.jpg")
+I6 = pygame.image.load("Assets/Next_Button.jpg")
+I7 = pygame.image.load("Assets/Previous_Button.jpg")
+
+###PLAY
+I9 = pygame.image.load("Assets/Play/2players.png")
+I10 = pygame.image.load("Assets/Play/3players.png")
+I11 = pygame.image.load("Assets/Play/4players.png")
+ID1 = pygame.image.load("Assets/Play/dice1.png")
+ID2 = pygame.image.load("Assets/Play/dice2.png")
+ID3 = pygame.image.load("Assets/Play/dice3.png")
+ID4 = pygame.image.load("Assets/Play/dice4.png")
+ID5 = pygame.image.load("Assets/Play/dice5.png")
+ID6 = pygame.image.load("Assets/Play/dice6.png")
