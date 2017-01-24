31303cb9a2fbca3c78f0fa1b4e95c0bf4fed82e7
RensMo <0934908@hr.nl>
2017-01-24 15:04:40 +0100

    Grid aangepast links recht

--- Combine Grid and Opseilen/Tile.py
+++ Combine Grid and Opseilen/Tile.py
@@ -5,7 +5,7 @@ class Position:
     def __init__(self, x, y):
         self.X = x
         self.Y = y
-    # test test
+
 class Tile:
     def __init__(self, colour, position):
         self.Colour = colour
@@ -23,19 +23,21 @@ class Tile:
                           (margin_y + height) * self.Position.Y + margin_y,
                           width,
                           height))
-        if self.Right != None:
+        if self.Right.Position.X != 0:
             self.Right.Draw(screen, width, height)
 
-        if self.Position.X == 0 and self.Down != None:
-            self.Down.Draw(screen, width, height)
+        if self.Position.X == 0 and self.Up != None:
+            self.Up.Draw(screen, width, height)
 
 
-def build_square_matrix(width, height):
+def build_matrix(width, height):
     entry_point = None
-    above_line = None
+    under_line = None
     prev_node = None
+    most_left_node = None
     for line in range(height):
         for tile in range(width):
+            # Assign colours
             if tile <= 1 * (width // 4) - 1:
                 colour = (255, 0, 0)  # red
             elif tile <= 2 * (width // 4) - 1:
@@ -45,22 +47,31 @@ def build_square_matrix(width, height):
             else:
                 colour = (0, 0, 255)  # blue
 
+            # Create Node
             node = Tile(colour, Position(tile, line))
 
-            if line == 0 and tile == 0:
+            if line == 0 and tile == 0:  # Define entry point
                 entry_point = node
+
             if tile == 0:
                 prev_node = node
-            else:
-                prev_node.Right = node
+                most_left_node = node
+            elif tile == width - 1:
                 node.Left = prev_node
+                most_left_node.Left = node
+                node.Right = most_left_node
+                prev_node.Right = node
                 prev_node = node
-            if line > 0:
-                node.Up = above_line
-                above_line.Down = node
-                above_line = above_line.Right
+            else:
+                node.Left = prev_node
+                prev_node.Right = node
+                prev_node = node
 
-        while prev_node.Left != None:
-            prev_node = prev_node.Left
-        above_line = prev_node
+            if line > 0:
+                node.Down = under_line  # Define .Down
+                under_line.Up = node  # Define .up
+                under_line = under_line.Right
+
+        prev_node = most_left_node
+        under_line = prev_node
     return entry_point
