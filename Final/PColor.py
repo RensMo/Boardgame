9112fa3539cc6bad870b9f9e9988f99bd75fcda1
Bahadir Aliyazicioglu <0932710@hr.nl>
2017-01-26 17:57:17 +0100

    minor fixes

--- Final/PColor.py
+++ Final/PColor.py
@@ -5,7 +5,7 @@ class PColor:
         self.x = x
         self.y = y
         self.sx = (x * 0.49) // 2
-        self.sy = x * 0.05
+        self.sy = x * 0.7
         self.colors = [(255,0,0), (0,255,0), (0,0,255), (255,255,0)]
         self.cc = 0
         self.color = self.colors[self.cc]
@@ -16,7 +16,7 @@ class PColor:
         self.x = x
         self.y = y
         self.sx = (x * 0.49) // 2
-        self.sy = x * 0.05
+        self.sy = x * 0.07
         self.rect1 = pygame.Rect((x, y), (self.sx, self.sy))
         self.rect2 = pygame.Rect((self.x + self.sx, y), (self.sx, self.sy))
         self.color = self.colors[self.cc]
