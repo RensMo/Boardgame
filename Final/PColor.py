a2a94840b504229d86cac1453d568c1637487ed5
Endy Hu <0937452@hr.nl>
2017-01-26 13:11:10 +0100

    Add files via upload

---
+++ Final/PColor.py
@@ -0,0 +1,26 @@
+import pygame
+
+class PColor:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.sx = (x * 0.15) // 2
+        self.sy = x * 0.05
+        self.colors = [(255,0,0), (0,255,0), (0,0,255), (255,255,0)]
+        self.cc = 0
+        self.color = self.colors[self.cc]
+        self.rect1 = pygame.Rect((x, y), (self.sx, self.sy))
+        self.rect2 = pygame.Rect((self.x + self.sx, y), (self.sx, self.sy))
+
+    def update(self, x, y):
+        self.x = x
+        self.y = y
+        self.sx = (x * 0.15) // 2
+        self.sy = x * 0.05
+        self.rect1 = pygame.Rect((x, y), (self.sx, self.sy))
+        self.rect2 = pygame.Rect((self.x + self.sx, y), (self.sx, self.sy))
+        self.color = self.colors[self.cc]
+
+    def draw(self, surface):
+        surface.fill(self.color, self.rect1)
+        surface.fill(self.color, self.rect2)
