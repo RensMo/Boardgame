7a6bcfb53c3f4860b71370e0a4623f83cb373656
Laurusl <0844052@hr.nl>
2017-01-27 14:29:13 +0100

    Update Questions.py

--- Final/Questions.py
+++ Final/Questions.py
@@ -1,33 +1,56 @@
+
 import mysql
 from mysql.connector import errorcode
 
-def question(category):
-    db = {
-    'user': 'Boardgame',
-    'password': 'groep12017',
-    'host': 'rens.xyz',
-    'database': 'Boardgame',
-    'raise_on_warnings': True,
+db = {
+'user': 'Boardgame',
+'password': 'groep12017',
+'host': '5.79.70.63',
+'database': 'Boardgame',
+'raise_on_warnings': True,
+}
+
+cnx = mysql.connector.connect(**db)
+cursor = cnx.cursor()
+
+if cnx.is_connected():
+    print("Connected to MYSQL database")
+
+
+class sql:
+
+    dbinfo = {
+        'user': 'Boardgame',
+        'password': 'groep12017',
+        'host': '5.79.70.63',
+        'database': 'Boardgame',
+        'raise_on_warnings': True,
     }
 
-    cnx = mysql.connector.connect(**db)
-    cursor = cnx.cursor()
+    def __init__(self):
+        self.db = mysql.connector.connect(**self.dbinfo)
 
-    if cnx.is_connected():
-        print("Connected to MYSQL database")
+    def query(self,sql):
+        cursor = self.db.cursor()
+        cursor.execute(sql)
+        return cursor.fetchall()
+
+    def rows(self):
+        return self.db.cursor().rowcount
+
+sqlI = sql()
+quest = sqlI.query("""select Question,Category,Right_answer, Wrong_answer, Wrong_answer2 from Vragen ORDER BY RAND() LIMIT 1""")
+
+class questions:
+    def __init__(self):
+        self.Questions = quest[0][0]
+        self.categ = quest[0][1]
+        self.ranswer = quest[0][2]
+        self.wanswer = quest[0][3]
+        self.wansert = quest[0][4]
 
 
-    cursor.execute("""select Question,Category,Right_answer, Wrong_answer, Wrong_answer2 from Vragen ORDER BY RAND() LIMIT 1""")
+questions()
 
-    result = cursor.fetchall()
-    cursor.close()
-    cnx.close()
 
-    question = result[0][0]
-    right_answer = result[0][2]
-    wrong_answer1 = result[0][3]
-    wrong_answer2 = result[0][4]
 
-    print(question, right_answer, wrong_answer1, wrong_answer2)
-
-question("Geography")
