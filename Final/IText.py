7b74366c113cac78906c5bc563946e076fdc46c1
Bahadir Aliyazicioglu <0932710@hr.nl>
2017-01-27 12:12:14 +0100

    minor fixes

--- Final/IText.py
+++ Final/IText.py
@@ -1,5 +1,19 @@
 import pygame
 
+class Textbg:
+    def __init__(self, x, y, sx, sy, I):
+        self.x = x
+        self.y = y
+        self.sx = sx
+        self.sy = sy
+        self.I = I
+        self.I = pygame.transform.scale(self.I, (int(sx), int(sy)))
+        self.rect = pygame.Rect((x,y), (sx,sy))
+        self.screct = pygame.Surface((int(sx), int(sy)))
+
+    def draw(self, surface):
+        surface.blit(self.I, (self.rect))
+
 class IText:
     def __init__(self, x, y, btext, size, width, color=pygame.Color("black")):
         self.x = x
