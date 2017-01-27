3414564d79ab774c1ebe33d84476218d38c7a0e7
Endy Hu <0937452@hr.nl>
2017-01-27 16:52:57 +0100

    Update Button.py

--- Final/Button.py
+++ Final/Button.py
@@ -1,5 +1,7 @@
 import pygame
 
+from Image import *
+
 class Button:
     def __init__(self, x, y, sx, sy, I):
         self.x = x
@@ -40,3 +42,31 @@ class Button2:
             self.I = pygame.transform.scale(self.I, (int(self.sx * 1.02), int(self.sy * 1.02)))
             self.rect = pygame.Rect((int(self.x - (self.sx * 1.02 - self.sx)), int(self.y - (self.sy * 1.02 - self.sy))), (int(self.sx), int(self.sy)))
             surface.blit(self.I, (self.rect))
+
+class Button3:
+    def __init__(self, x, y, sx, sy):
+        self.x = x
+        self.y = y
+        self.sx = sx
+        self.sy = sy
+        self.Is = [S3, S4]
+        self.cc = 0
+        self.I = self.Is[self.cc]
+        self.I = pygame.transform.scale(self.I, (int(self.sx), int(self.sy)))
+        self.irect = pygame.Rect((x,y), (self.sx, self.sy))
+        self.rect1 = pygame.Rect((x,y), (self.sx * 0.5, self.sy))
+        self.rect2 = pygame.Rect((self.x + self.sx * 0.5, y), (self.sx * 0.5, self.sy))
+
+    def update(self, x, y, sx, sy):
+        self.x = x
+        self.y = y
+        self.sx = sx
+        self.sy = sy
+        self.I = self.Is[self.cc]
+        self.I = pygame.transform.scale(self.I, (int(self.sx), int(self.sy)))
+        self.irect = pygame.Rect((x,y), (self.sx, self.sy))
+        self.rect1 = pygame.Rect((x,y), (self.sx * 0.5, self.sy))
+        self.rect2 = pygame.Rect((self.x + self.sx * 0.5, y), (self.sx * 0.5, self.sy))
+
+    def draw(self, surface):
+        surface.blit(self.I, (self.irect))
