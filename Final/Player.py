80b12fde6d735f87cec24d7cab782391ff74f55a
Luciano <0923431@hr.nl>
2017-01-31 17:06:53 +0100

    Saves

--- Final/Player.py
+++ Final/Player.py
@@ -9,15 +9,10 @@ class Player:
         self.Colour = (255, 255, 255)
         self.StartCat = None
 
-    def save(self):
-        PIK = "Save1.txt"
-        data = ['A', 'B', 'C', 'D']
-        with open(PIK, "wb") as f:
-            pickle.dump(data, f)
-        with open(PIK, "rb") as f:
-            print(pickle.load(f))
+Game_data = {Player}
+pickle.dump(Game_data, open("Save1.txt", "wb"))
 
-    def Draw(self, screen, width, height, pos_x, pos_y):
+def Draw(self, screen, width, height, pos_x, pos_y):
         margin_x = 0.1 * width
         margin_y = 0.1 * height
         pygame.draw.ellipse(screen, self.Colour,
