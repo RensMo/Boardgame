999b3590b1a888d2d9fa812482fb9991a6c02b7c
Bahadir Aliyazicioglu <0932710@hr.nl>
2017-01-31 15:02:06 +0100

    new bg music

--- Final/Sounds.py
+++ Final/Sounds.py
@@ -1,6 +1,6 @@
 import pygame
 pygame.init()
-pygame.mixer.music.load("Instrumental.mp3")
+pygame.mixer.music.load("Assets/Instrumental.mp3")
 pygame.mixer.music.play(-1)
 
 Sound1 = pygame.mixer.Sound("Assets/click.wav")
