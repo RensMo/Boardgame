94e985e7e5a51c5c625ffd6f0e1005ffe4850610
Laurusl <0844052@hr.nl>
2017-02-01 02:31:31 +0100

    Update Highscore.py

--- Final/Highscore.py
+++ Final/Highscore.py
@@ -3,20 +3,35 @@ import MySQLdb
 def High_score(Name_player,High_score,Player_wins,Player_loses,Win_loss_ratio):
     query = ( "INSERT INTO High_Score VALUES (%s, %s, %s, %s, %s)")
     print(query)
-    # Open database connection
+    
     db = MySQLdb.connect("5.79.70.63", "Boardgame", "groep12017", "Boardgame")
 
     cursor = db.cursor()
 
     cursor.execute(query, (Name_player,High_score,Player_wins,Player_loses,Win_loss_ratio))
-    #data = cursor.commit()
+  
     db.commit()
 
-    #result = data
-
-    # disconnect from server
+   
     db.close()
 
 
-High_score('player1',185,5,6,5)
 
+
+
+def Overview_score():
+    query = """SELECT * FROM High_Score ORDER BY High_score DESC LIMIT 10"""
+    
+    db = MySQLdb.connect("5.79.70.63", "Boardgame", "groep12017", "Boardgame")
+
+    cursor = db.cursor()
+
+    cursor.execute(query)
+    data = cursor.fetchall()
+
+    result = data
+    db.close()
+
+    return result
+
+Overview_score()
