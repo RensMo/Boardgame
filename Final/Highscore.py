f69454f61a0713182e26eb767a3e8589f637f8e8
Laurusl <0844052@hr.nl>
2017-02-01 16:10:57 +0100

    Update Highscore.py

--- Final/Highscore.py
+++ Final/Highscore.py
@@ -5,7 +5,6 @@ def High_score(Name_player,High_score,Player_wins,Player_loses,Win_loss_ratio):
     print(query)
     
     db = MySQLdb.connect("5.79.70.63", "Boardgame", "groep12017", "Boardgame")
-
     cursor = db.cursor()
 
     cursor.execute(query, (Name_player,High_score,Player_wins,Player_loses,Win_loss_ratio))
@@ -34,4 +33,20 @@ def Overview_score():
 
     return result
 
-Overview_score()
+print(Overview_score())
+
+Score = Overview_score()
+
+
+resultLines = []
+
+
+playeramount = 10
+for i in range(playeramount):
+    resultLines.append("%s %i %i %i %i" % (Score[i][0], Score[i][1], Score[i][2], Score[i][3], Score[i][4]))
+
+
+print(resultLines)
+
+
+
