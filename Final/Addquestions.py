8533e0df94acd75ac44beba5f3a7b9262e14bcbe
Laurusl <0844052@hr.nl>
2017-02-01 15:56:23 +0100

    Update Addquestions.py

--- Final/Addquestions.py
+++ Final/Addquestions.py
@@ -1,7 +1,6 @@
-import MySQLdb
-
 def Add_question(Q_id,Question,Category,Right_answer,Wrong_answer,Wrong_answer2):
-    query = ( "INSERT INTO Vragen VALUES (%s, %s, %s, %s, %s)")
+    query = ( "INSERT INTO Vragen VALUES (%s, %s, %s, %s, %s, %s)")
+
     print(query)
     # Open database connection
     db = MySQLdb.connect("5.79.70.63", "Boardgame", "groep12017", "Boardgame")
@@ -15,24 +14,3 @@ def Add_question(Q_id,Question,Category,Right_answer,Wrong_answer,Wrong_answer2)
 
     # disconnect from server
     db.close()
-
-
-Add_question(ID,vraag,Category,Goede antwoord,Foute antwoord1, Foute antwoord2)
-
-
-Question_nr = 21
-Question_nr += 1
-
-class Add_questions:
-    def __init__(self, x, y, size):
-
-        self.x = x
-        self.y = y
-        self.size = size
-
-    def draw(self, surface):
-
-        surface.blit(self., (self.x, self.y))
-        surface.blit(self., (self.x, self.y + self.y * 0.2))
-        surface.blit(self., (self.x, self.y + self.y * 0.4))
-        surface.blit(self., (self.x, self.y + self.y * 0.6))
