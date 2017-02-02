4c53d32559892d01addbfd2502dfbc9bf750169f
Luciano <0923431@hr.nl>
2017-02-02 16:08:17 +0100

    .

--- Savegames.py
+++ Savegames.py
@@ -1,5 +1,20 @@
-import cPickle as pickle
+import pickle
 
+class Player:
+    def __init__(self, name, tile):
+        self.Name = name
+        self.Tile = tile
+        self.Colour = (255, 255, 255)
+        self.StartCat = None
+
+    def Draw(self, screen, width, height, pos_x, pos_y):
+            margin_x = 0.1 * width
+            margin_y = 0.1 * height
+            pygame.draw.ellipse(screen, self.Colour,
+                                (pos_x + (margin_x + width) * self.Tile.Position.X + margin_x,
+                                 pos_y + (-margin_y - height) * self.Tile.Position.Y - margin_y,
+                                 width,
+                                 height))
 def saveGame():
     allNames = []
     for sprite in alites:
